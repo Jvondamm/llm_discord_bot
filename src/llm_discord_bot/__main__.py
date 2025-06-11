d7da82efa5369955d23f6f9e3d6783cffcf769bd
joshuavd <joshuavd@att.net>
2025-06-10 22:49:08 -0700

    changed main path

--- src/llm_discord_bot/__main__.py
+++ src/llm_discord_bot/__main__.py
@@ -7,7 +7,7 @@ from llm_discord_bot.llmrag import LlmRag
 
 def main():
     load_dotenv()
-    bot = Bot(llm=LlmRag(), config_file=Path(__file__).parent.parent / 'config.json')
+    bot = Bot(llm=LlmRag(), config_file=Path(__file__).parent.parent.parent / 'config.json')
     bot.run(os.getenv("DISCORD_TOKEN"))
 
 
