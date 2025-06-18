3fc1d20b05d01f2ac421d8935606e96cebefe922
joshuavd <joshuavd@att.net>
2025-06-18 09:49:34 -0700

    trying to get working on mac without cuda and fixing logging

--- src/llm_discord_bot/__init__.py
+++ src/llm_discord_bot/__init__.py
@@ -1,1 +1,6 @@
+import logging
 __version__ = "0.0.1"
+__log_format__ = "%(asctime)s.%(msecs)%02d %(levelname)-8s %(name)-15s [%(filename)s:%(lineno)d] %(message)s"
+__log_date_format__ = "%Y-%m-%d %H:%M:%S"
+
+logging.basicConfig(format=__log_format__, datefmt=__log_date_format__)
